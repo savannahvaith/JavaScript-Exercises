f783e340ea4d34a3602337360fa4d3daf97200e6
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-04-23 13:31:10 +0100

    API Call Example finished

--- 10_API_Calls/02/index.js
+++ 10_API_Calls/02/index.js
@@ -3,18 +3,19 @@ const REQ = new XMLHttpRequest();
 function getTodos() {
     REQ.onload = () => {
         if (REQ.status === 200) {
-            console.log(REQ);
+            // console.log(REQ);
             console.log(REQ.response);
-            // document.querySelector('#resp').innerHTML = REQ.response;
+            console.log(REQ.response.title);
+            document.querySelector('#resp').innerHTML = REQ.response[0].title;
         } else {
             console.log(`Handle Error!`);
         }
     }
-    REQ.open('GET', 'http://jsonplaceholder.typicode.com/todos/1');
+    REQ.open('GET', 'http://jsonplaceholder.typicode.com/todos/');
     REQ.setRequestHeader('Content-Type', 'Application/json');
     REQ.setRequestHeader('Access-Control-Allow-Origin', '*');
     REQ.responseType = 'json';
-    REQ.send("Hello");
+    REQ.send();
 }
 
 let butt1 = document.querySelector('#butt1');
