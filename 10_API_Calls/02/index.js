2bde85821b9c384f97e65a12a10829d9cad5b455
Savannah Vaith <sav.vaithilingam@gmail.com>
2020-04-23 14:37:22 +0100

    API POST Method implemented

--- 10_API_Calls/02/index.js
+++ 10_API_Calls/02/index.js
@@ -20,3 +20,25 @@ function getTodos() {
 
 let butt1 = document.querySelector('#butt1');
 butt1.addEventListener('click', getTodos);
+
+
+// POSTING INFORMATION TO THE SERVER
+let data = '{"title": "foo","body": "bar","userId": 1}';
+function postTodo() {
+    REQ.onload = () => {
+        if (REQ.status === 201) {
+            console.log(REQ.response);
+        } else {
+            console.log('handle error');
+        }
+    }
+    REQ.open('POST', 'http://jsonplaceholder.typicode.com/posts');
+    REQ.setRequestHeader('Content-Type', 'Application/json');
+    REQ.setRequestHeader('Access-Control-Allow-Origin', '*');
+    REQ.send(data); // Waht we want to send across
+}
+
+let butt2 = document.querySelector('#butt2');
+butt2.addEventListener('click', postTodo);
+
+
